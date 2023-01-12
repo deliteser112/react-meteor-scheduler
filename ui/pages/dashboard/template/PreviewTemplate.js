bcc3604a28db7aad5707413e2941745414d4a7be
deliteser112 <74626440+deliteser112@users.noreply.github.com>
2023-01-13 01:48:49 +0900

    preview scheduler done

--- ui/pages/dashboard/template/PreviewTemplate.js
+++ ui/pages/dashboard/template/PreviewTemplate.js
@@ -58,7 +58,7 @@ const Item = styled.div`
   display: flex;
   user-select: none;
   padding: 0;
-  margin: 0 0 0 0;
+  margin: 0;
   align-items: flex-start;
   align-content: flex-start;
   line-height: 1.5;
@@ -108,35 +108,6 @@ const Notice = styled.div`
   color: #aaa;
   user-select: none;
 `;
-
-const ITEMS = [
-  {
-    _id: 'item_0',
-    id: uuid(),
-    content: 'Olgica Krsteva'
-  },
-  {
-    _id: 'item_1',
-    id: uuid(),
-    content: 'Daniel Lewis'
-  },
-  {
-    _id: 'item_2',
-    id: uuid(),
-    content: 'Alexander Rydin'
-  },
-  {
-    _id: 'item_3',
-    id: uuid(),
-    content: 'Viktoriia Chubatiuk'
-  },
-  {
-    _id: 'item_4',
-    id: uuid(),
-    content: 'Illia Voloshenko'
-  }
-];
-
 function PreviewTemplate() {
   const [scheduleData, setScheduleData] = useState([]);
 
@@ -152,7 +123,7 @@ function PreviewTemplate() {
   };
 
   useEffect(() => {
-    const tables = generateTable(15, 7);
+    const tables = generateTable(AREAS.length * DAYS.length, SESSIONS.length);
     setScheduleData([...tables]);
   }, []);
 
@@ -216,7 +187,7 @@ function PreviewTemplate() {
             <div>
               <label>
                 <input type="checkbox" checked={isBlocking} onChange={handleIsBlocking} style={{ marginBottom: 20 }} />
-                Blocked Out
+                Blocked Out - 1
               </label>
             </div>
             <div>
@@ -253,82 +224,221 @@ function PreviewTemplate() {
         )}
       </Droppable>
       <Content>
-        {scheduleData.map((rows, rowIdx) => {
-          return (
-            <div key={`row_${rowIdx}`} style={{ display: 'flex' }}>
-              {rows.map(({ list, isBlocked }, colIdx) => {
-                return (
-                  <Droppable key={`${rowIdx}_${colIdx}`} droppableId={`${rowIdx}_${colIdx}`} isDropDisabled={isBlocked}>
-                    {(provided, snapshot) => (
-                      <Cell
-                        innerRef={provided.innerRef}
-                        isDraggingOver={snapshot.isDraggingOver}
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: '0 0 300px' }} />
+          {SESSIONS.map((item) => (
+            <Cell key={item._id} style={{ backgroundColor: '#FFC7CE', color: '#9C0006' }}>
+              {item.title}
+            </Cell>
+          ))}
+        </div>
+        <div style={{ display: 'flex' }}>
+          <div style={{ flex: '0 0 300px' }}>
+            {DAYS.map((day) => (
+              <div key={day._id} style={{ display: 'flex' }}>
+                <div style={{ flex: '0 0 150px', borderTop: '1px solid #aaa' }}>{day.title}</div>
+                <div style={{ flex: '0 0 150px' }}>
+                  {AREAS.map((area) => (
+                    <Cell key={area._id} style={{ backgroundColor: '#C6EFCE' }}>
+                      <Notice style={{ color: '#006100' }}>{area.title}</Notice>
+                    </Cell>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>
+            {scheduleData.map((rows, rowIdx) => {
+              return (
+                <div key={`row_${rowIdx}`} style={{ display: 'flex' }}>
+                  {rows.map(({ list, isBlocked }, colIdx) => {
+                    return (
+                      <Droppable
+                        key={`${rowIdx}_${colIdx}`}
+                        droppableId={`${rowIdx}_${colIdx}`}
                         isDropDisabled={isBlocked}
-                        onClick={() => {
-                          if (isBlocking) {
-                            const newScheData = [...scheduleData];
-                            const blockedOut = newScheData[rowIdx][colIdx].isBlocked;
-                            newScheData[rowIdx][colIdx].isBlocked = !blockedOut;
-                            setScheduleData([...newScheData]);
-                          }
-                        }}
                       >
-                        {list.length
-                          ? list.map((item, index) => (
-                              <Draggable key={item.id} draggableId={item.id} index={index}>
-                                {(provided, snapshot) => (
-                                  <Item
-                                    innerRef={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    isDragging={snapshot.isDragging}
-                                    style={provided.draggableProps.style}
-                                  >
-                                    <svg
-                                      onClick={() => {
-                                        const newScheData = [...scheduleData];
-                                        newScheData[rowIdx][colIdx].list.splice(index, 1);
-                                        setScheduleData(newScheData.filter((group) => group.length));
-                                      }}
-                                      width="18"
-                                      height="18"
-                                      viewBox="0 0 18 18"
-                                      style={{
-                                        cursor: 'pointer'
-                                      }}
-                                    >
-                                      <path
-                                        fill="currentColor"
-                                        d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6Z"
-                                      />
-                                    </svg>
-                                    <div
-                                      style={{
-                                        maxWidth: 135,
-                                        whiteSpace: 'nowrap',
-                                        overflow: 'hidden',
-                                        textOverflow: 'ellipsis'
-                                      }}
-                                    >
-                                      {item.content}
-                                    </div>
-                                  </Item>
-                                )}
-                              </Draggable>
-                            ))
-                          : !provided.placeholder && <Notice>.</Notice>}
-                        {provided.placeholder}
-                      </Cell>
-                    )}
-                  </Droppable>
-                );
-              })}
-            </div>
-          );
-        })}
+                        {(provided, snapshot) => (
+                          <Cell
+                            innerRef={provided.innerRef}
+                            isDraggingOver={snapshot.isDraggingOver}
+                            isDropDisabled={isBlocked}
+                            onClick={() => {
+                              if (isBlocking) {
+                                const newScheData = [...scheduleData];
+                                const blockedOut = newScheData[rowIdx][colIdx].isBlocked;
+                                newScheData[rowIdx][colIdx].isBlocked = !blockedOut;
+                                setScheduleData([...newScheData]);
+                              }
+                            }}
+                          >
+                            {list.length
+                              ? list.map((item, index) => (
+                                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                                    {(provided, snapshot) => (
+                                      <Item
+                                        innerRef={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        isDragging={snapshot.isDragging}
+                                        style={provided.draggableProps.style}
+                                      >
+                                        <svg
+                                          onClick={() => {
+                                            const newScheData = [...scheduleData];
+                                            newScheData[rowIdx][colIdx].list.splice(index, 1);
+                                            setScheduleData(newScheData.filter((group) => group.length));
+                                          }}
+                                          width="18"
+                                          height="18"
+                                          viewBox="0 0 18 18"
+                                          style={{
+                                            cursor: 'pointer'
+                                          }}
+                                        >
+                                          <path
+                                            fill="currentColor"
+                                            d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6Z"
+                                          />
+                                        </svg>
+                                        <div
+                                          style={{
+                                            maxWidth: 135,
+                                            whiteSpace: 'nowrap',
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis'
+                                          }}
+                                        >
+                                          {item.content}
+                                        </div>
+                                      </Item>
+                                    )}
+                                  </Draggable>
+                                ))
+                              : !provided.placeholder && <Notice>-</Notice>}
+                            {provided.placeholder}
+                          </Cell>
+                        )}
+                      </Droppable>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </Content>
     </DragDropContext>
   );
 }
 
 export default PreviewTemplate;
+
+const ITEMS = [
+  {
+    _id: 'item_0',
+    id: uuid(),
+    content: 'Olgica Krsteva'
+  },
+  {
+    _id: 'item_1',
+    id: uuid(),
+    content: 'Daniel Lewis'
+  },
+  {
+    _id: 'item_2',
+    id: uuid(),
+    content: 'Alexander Rydin'
+  },
+  {
+    _id: 'item_3',
+    id: uuid(),
+    content: 'Viktoriia Chubatiuk'
+  },
+  {
+    _id: 'item_4',
+    id: uuid(),
+    content: 'Illia Voloshenko'
+  }
+];
+
+const SESSIONS = [
+  {
+    _id: 1,
+    title: 'Before School 1'
+  },
+  {
+    _id: 2,
+    title: 'Before School 2'
+  },
+  {
+    _id: 3,
+    title: 'Recess 1'
+  },
+  {
+    _id: 4,
+    title: 'Recess 2'
+  },
+  {
+    _id: 5,
+    title: 'Lunch 1'
+  },
+  {
+    _id: 6,
+    title: 'Lunch 2'
+  },
+  {
+    _id: 7,
+    title: 'After School'
+  }
+];
+
+const AREAS = [
+  {
+    _id: 1,
+    title: 'Car Park Gate'
+  },
+  {
+    _id: 2,
+    title: 'Courtyart'
+  },
+  {
+    _id: 3,
+    title: 'Front Gate'
+  },
+  {
+    _id: 4,
+    title: 'Junior Playground'
+  },
+  {
+    _id: 5,
+    title: 'Learning Street'
+  },
+  {
+    _id: 61,
+    title: 'Basketball Court'
+  }
+];
+
+const DAYS = [
+  {
+    _id: 1,
+    title: 'Monday'
+  },
+  {
+    _id: 2,
+    title: 'Tuesday'
+  },
+  {
+    _id: 3,
+    title: 'Wednesday'
+  },
+  {
+    _id: 4,
+    title: 'Thursday'
+  },
+  {
+    _id: 5,
+    title: 'Friday'
+  }
+];
